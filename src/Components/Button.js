bbc90216a40ca3c9bf86d9fd10796769d37a46c0
Antonio Hadrović <hadrovic1@gmail.com>
2020-06-29 15:41:03 +0200

    Roadblock 1

--- src/Components/Button.js
+++ src/Components/Button.js
@@ -3,7 +3,7 @@ import React from 'react';
 
 export default function Button(props) {
     return (
-        <button className='button'>
+        <button type="button" className='button' onClick=""> 
             <p className=''>{props.value}</p>
         </button>
     )
