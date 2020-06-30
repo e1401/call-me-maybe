a2649ffdc53e8aef50f7b49e82daacc7748614dc
Antonio Hadrović <hadrovic1@gmail.com>
2020-06-30 20:30:41 +0200

    Not good

--- src/Components/Keypad.js
+++ src/Components/Keypad.js
@@ -1,51 +1,21 @@
-import React from 'react';
+import React from "react";
 import Button from './Button';
 import CallButton from './CallButton';
 import HangUpButton from './HangUpButton'
 
 
-// export default  function Keypad() {
 
-
-//     return (
-//         <div className='keypad'>
-//                 <div className="keyRow">
-//                 <CallButton />
-//                 <HangUpButton />
-                
-//             </div>
-//             <div className="keyRow">
-//                 <Button value={this.state.value}></Button>
-//                 <Button number="2"></Button>
-//                 <Button number="3"></Button>
-//             </div>
-//             <div className="keyRow">
-//                 <Button number="4"></Button>
-//                 <Button number="5"></Button>
-//                 <Button number="6"></Button>
-//             </div>
-//             <div className="keyRow">
-//                 <Button number="7"></Button>
-//                 <Button number="8"></Button>
-//                 <Button number="9"></Button>
-//             </div>
-//             <div className="keyRow">
-//                 <Button number="*"></Button>
-//                 <Button number="0"></Button>
-//                 <Button number="#"></Button>
-//             </div>
-//         </div>
-//     )
-// }
 
 
 export default class Keypad extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            value: ['1', '2', '3', '4', '5', '6', '7', '8', '9', '*', '0', '#'],
-            
-        }
+   
+     
+   
+
+    handleChange(event) {
+       
+        this.props.onKeyPress(event.target.value);
+        
     }
 
     render () {
@@ -57,30 +27,28 @@ export default class Keypad extends React.Component {
             
         </div>
         <div className="keyRow">
-            <Button value={this.state.value[0]}></Button>
-            <Button value={this.state.value[1]}></Button>
-            <Button value={this.state.value[2]}></Button>
+            <Button type="button"  value={this.props.value[0]}></Button>
+            <Button value={this.props.value[1]}></Button>
+            <Button value={this.props.value[2]}></Button>
         </div>
         <div className="keyRow">
-            <Button value={this.state.value[3]}></Button>
-            <Button value={this.state.value[4]}></Button>
-            <Button value={this.state.value[5]}></Button>
+            <Button value={this.props.value[3]}></Button>
+            <Button value={this.props.value[4]}></Button>
+            <Button value={this.props.value[5]}></Button>
         </div>
         <div className="keyRow">
-            <Button value={this.state.value[6]}></Button>
-            <Button value={this.state.value[7]}></Button>
-            <Button value={this.state.value[8]}></Button>
+            <Button value={this.props.value[6]}></Button>
+            <Button value={this.props.value[7]}></Button>
+            <Button value={this.props.value[8]}></Button>
         </div>
         <div className="keyRow">
-            <Button value={this.state.value[9]}></Button>
-            <Button value={this.state.value[10]}></Button>
-            <Button value={this.state.value[11]}></Button>
+            <Button value={this.props.value[9]}></Button>
+            <Button value={this.props.value[10]}></Button>
+            <Button value={this.props.value[11]}></Button>
         </div>
 
         </div>
         )
     }
+
 }
-
-
-// TODO: LIFTING STATE UP https://reactjs.org/docs/lifting-state-up.html
--- src/Components/Phone.js
+++ src/Components/Phone.js
@@ -3,6 +3,19 @@ import PhoneScreen from './PhoneScreen'
 import Keypad from './Keypad'
 
 export default class Phone extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            value: ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '*', '#'],
+            
+        }
+    }
+
+    // handleClick(event) {
+    //     this.setState({ this.stae.value.map(v =)});
+    //     event.preventDefault();
+    //   }; NOT GOOD
+
     render () {
         return (
             <div className="phoneBase">
