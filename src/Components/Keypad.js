d7531fc3206276c16ee2c46194e116406d73cf9e
Antonio Hadrovic <antoniohadrovic@Antonios-iMac.local>
2020-07-01 14:24:05 +0200

    Move state to Keypad componet

--- src/Components/Keypad.js
+++ src/Components/Keypad.js
@@ -9,12 +9,19 @@ import HangUpButton from './HangUpButton'
 
 export default class Keypad extends React.Component {
    
-     
+    constructor(state) {
+        super(state);
+        this.state = {
+            value: ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '*', '#'],
+            
+        }
+    }
+
    
 
     handleChange(event) {
        
-        this.props.onKeyPress(event.target.value);
+        this.state.onKeyPress(event.target.value);
         
     }
 
@@ -27,24 +34,24 @@ export default class Keypad extends React.Component {
             
         </div>
         <div className="keyRow">
-            <Button type="button"  value={this.props.value[0]}></Button>
-            <Button value={this.props.value[1]}></Button>
-            <Button value={this.props.value[2]}></Button>
+            <Button type="button"  value={this.state.value[0]}></Button>
+            <Button value={this.state.value[1]}></Button>
+            <Button value={this.state.value[2]}></Button>
         </div>
         <div className="keyRow">
-            <Button value={this.props.value[3]}></Button>
-            <Button value={this.props.value[4]}></Button>
-            <Button value={this.props.value[5]}></Button>
+            <Button value={this.state.value[3]}></Button>
+            <Button value={this.state.value[4]}></Button>
+            <Button value={this.state.value[5]}></Button>
         </div>
         <div className="keyRow">
-            <Button value={this.props.value[6]}></Button>
-            <Button value={this.props.value[7]}></Button>
-            <Button value={this.props.value[8]}></Button>
+            <Button value={this.state.value[6]}></Button>
+            <Button value={this.state.value[7]}></Button>
+            <Button value={this.state.value[8]}></Button>
         </div>
         <div className="keyRow">
-            <Button value={this.props.value[9]}></Button>
-            <Button value={this.props.value[10]}></Button>
-            <Button value={this.props.value[11]}></Button>
+            <Button value={this.state.value[9]}></Button>
+            <Button value={this.state.value[10]}></Button>
+            <Button value={this.state.value[11]}></Button>
         </div>
 
         </div>
