0ffe6cf4b63f6a58efa22182075ebf5168f2a7f7
Antonio Hadrović <hadrovic1@gmail.com>
2020-07-26 15:23:23 +0200

    Updates

---
+++ precache-manifest.3c6373371ce26ea3b79b2593af82975c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "04bffd94b97135577f6675995efe5516",
+    "url": "/call-me-maybe/index.html"
+  },
+  {
+    "revision": "473500a985e3d304b432",
+    "url": "/call-me-maybe/static/css/main.c80997f1.chunk.css"
+  },
+  {
+    "revision": "42774555c06f91234881",
+    "url": "/call-me-maybe/static/js/2.dd05908c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/call-me-maybe/static/js/2.dd05908c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "473500a985e3d304b432",
+    "url": "/call-me-maybe/static/js/main.b9d6ddb9.chunk.js"
+  },
+  {
+    "revision": "bba7f4b6e4b4d7597bef",
+    "url": "/call-me-maybe/static/js/runtime-main.4cb2a9ab.js"
+  }
+]);
